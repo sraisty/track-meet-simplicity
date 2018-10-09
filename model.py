f3ab94c77bb301676afa369ade0ba6a190eac85b
Sue Raisty <sraisty@gmail.com>
2018-10-09 09:50:56 +0000

    Basic data model might be working almost (except heats, seeding, results). Not thoroughly tested at all. But at least no blatant errors.

--- model.py
+++ model.py
@@ -9,16 +9,38 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Athlete(db.Model):
+class Meet(db.Model):
+    __tablename__ = "meets"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.DateTime, nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+    host_school_id = db.Column(db.ForeignKey("schools.id"), nullable=True)
+    sport_id = db.Column(db.ForeignKey("sports.id"),
+                         nullable=False)
+
+    sport = db.relationship("Sport")
+    host_school = db.relationship("School")
+    meet_division_events = db.relationship("MeetDivisionEvent")
+
+    ### HOW DO I GET THE LIST OF DIVISION IN THE MEET???
+    
+    ###divisions = db.relationship("MeetDivisionEvent.division")
+
+
+    def __repr__(self):
+        return "\nMEET# {}: {}".format(self.id, self.name)
+
+class Athlete(db.Model):
+    """ """
     __tablename__ = "athletes"
 
-    athlete_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fname = db.Column(db.String(30), nullable=False)
     lname = db.Column(db.String(30), nullable=False)
     minitial = db.Column(db.String(1), nullable=True)
-    school_id = db.Column(db.ForeignKey('schools.school_id'), nullable=False)
-    div_id = db.Column(db.ForeignKey('divisions.division_id'), nullable=False)
+    school_id = db.Column(db.ForeignKey('schools.id'), nullable=False)
+    div_id = db.Column(db.ForeignKey('divisions.id'), nullable=False)
 
     school = db.relationship("School")
     division = db.relationship("Division")
@@ -27,7 +49,7 @@ class Athlete(db.Model):
 
     def __repr__(self):
         return "\nATHL# {}: {} School: {}, Div: {}".format(
-                self.athlete_id,
+                self.id,
                 self.make_full_name(self.fname, self.minitial, self.lname),
                 self.school_id,
                 self.division.get_div_name())
@@ -45,16 +67,36 @@ class Athlete(db.Model):
 
         >>> Athlete.make_full_name("Jane", "", "Doe")
         'Jane Doe'
-
-        >>> Athlete.make_full_name("William", "H.", "Macy")
-        'William H. Macy'
-
-        >>> Athlete.make_full_name("William", "H", "Macy")
-        'William H. Macy'
         """
         if minitial:
             return f"{fname} {minitial[0]}. {lname}"
         return f"{fname} {lname}"
+
+
+class Entry(db.Model):
+    """
+    A middle table. The "entries" of athletes into MeetDivisionEvents. Each
+    athlete can enter multiple events at a meet, within his/her division.
+    Each division within an event has multiple athletes.
+
+    Note that entering into an event doesn't mean the athlete will actually
+    get assigned to the event
+    """
+    __tablename__ = "entries"
+    entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    athlete_id = db.Column(db.ForeignKey("athletes.id"),
+                           nullable=False)
+    meet_div_event_id = db.Column(db.ForeignKey("meet_division_events.id"),
+                                  nullable=False)
+    # mark = db.Column(db.Float, nullable=True)
+    # mark_date = db.Column(db.Datetime, nullable=True)
+
+    athlete = db.relationship("Athlete")
+    meet_div_event = db.relationship("MeetDivisionEvent")
+    # division = db.relationship("MeetDivisionEvent.division")
+    # event = db.relationship("MeetDivisionEvent.event")
+    # meet = db.relationship("MeetDivisionEvent.meet")
 
 
 class School(db.Model):
@@ -63,43 +105,128 @@ class School(db.Model):
     __tablename__ = "schools"
 
     # Set up so that the "Unattached" School always uses primary key #1
-    school_id = db.Column(db.Integer, primary_key=True, autoincrement=True,
-                          default=1)
-    school_name = db.Column(db.String(50), nullable=False, unique=True,
-                            default="Unattached")
-    school_city = db.Column(db.String(30), nullable=True)
-    school_state = db.Column(db.String(2), nullable=True)
-
+    id = db.Column(db.Integer, primary_key=True,
+                   autoincrement=True,
+                   default=1)
+    name = db.Column(db.String(50), nullable=False,
+                     unique=True,
+                     default="Unattached")
+    city = db.Column(db.String(30), nullable=True)
+    state = db.Column(db.String(2), nullable=True)
     athletes = db.relationship("Athlete")
 
     def __repr__(self):
-        return "SCHOOL ID#{}: {}".format(self.school_id, self.school_name)
+        return "SCHOOL ID#{}: {}".format(self.id, self.name)
+
+
+
+#######  THESE TABLES ARE INITIALIZED BUT NOT MODIFIED GOING FORWARD 
+####### CONTAIN "Constant" Data or are used for referential integrity
 
 
 class Division(db.Model):
     """
     """
     __tablename__ = "divisions"
-
-    division_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     gender_code = db.Column(db.ForeignKey("genders.gender_code"),
                             nullable=False)
     grade_code = db.Column(db.ForeignKey("grades.grade_code"), nullable=False)
+    # set up division's relationships to gender and grade tables
     gender = db.relationship("Gender")
     grade = db.relationship("Grade")
 
+
     def __repr__(self):
-        return "DIVISION ID#{}: {}".format(self.division_id,
+        """ returns human-readable representation of Division object """
+        return "DIVISION ID#{}: {}".format(self.id,
                                            self.get_div_name())
+
+    @classmethod
+    def get_division(cls, gender_string, grade_num):
+        """ Gets the division given the gender string (as "M" or "F"),
+        and the grade as a number. Returns null if no division exists that
+        matches the gender/grade combo.
+        """
+        division = cls.query.filter_by(gender_code=gender_string,
+                                       grade_code=str(grade_num)).first()
+        return division
+
 
     def get_div_name(self):
         return f"{self.grade.grade_name} {self.gender.gender_name}"
 
 
+class Event_Definition(db.Model):
+    """
+    """
+    __tablename__ = "event_defs"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    abbrev = db.Column(db.String(8), unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    event_type_id = db.Column(db.ForeignKey("event_def_types.id"),
+                              nullable=False)
+    type = db.relationship("Event_Def_Type")
+
+
+    def __repr__(self):
+        """
+        Returns human-readable representation of the Event_Definition object
+        """
+        return "EVENT_DEF ID#{}: {} - {}. Type: {}".format(
+                self.id,
+                self.abbrev,
+                self.name,
+                self.id)
+
+
+class Event_Def_Type(db.Model):
+    """
+    """
+    __tablename__ = "event_def_types"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(8), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "EVENT Def TypeID#{}: {}".format(self.id, self.name)
+
+    @classmethod
+    def get_from_name(cls, event_type_string):
+        return cls.query.filter(cls.name == event_type_string).one()
+
+
+class MeetDivisionEvent(db.Model):
+    """
+    Associative table to handle the many to many relationship between Events
+    and Divisions.
+    """
+    __tablename__ = "meet_division_events"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    div_id = db.Column(db.ForeignKey("divisions.id"), nullable=False)
+    meet_id = db.Column(db.ForeignKey("meets.id"), nullable=False)
+    event_def_id = db.Column(db.ForeignKey("event_defs.id"),
+                             nullable=False)
+
+    # event = db.relationship("Event")
+    division = db.relationship("Division")
+    entries = db.relationship("Entry")
+    meet = db.relationship("Meet")
+    event = db.relationship("Event_Definition")
+    # how do I get a relationship athletes
+
+    def __repr__(self):
+        return "\nMeetDivEvent#{}: Meet: '{}', Event: {}, Division: {}".format(
+                self.id,
+                self.meet.name,
+                self.event.abbrev,
+                self.division.get_div_name())
+
+
 class Gender(db.Model):
     """
-    In MVP, we only support 2 genders: 'M' and 'F', but there are sometimes
-    "Mixed" or "Non-Binary" gender divisions.
+    In MVP, we only support 2 genders: 'M' and 'F'
     """
     __tablename__ = "genders"
 
@@ -128,79 +255,21 @@ class Grade(db.Model):
         return "GRADE CODE#{}: {}".format(self.grade_code, self.grade_name)
 
 
-class Event(db.Model):
-    """
-    """
-    __tablename__ = "events"
+class Sport(db.Model):
+    """ The sports are Outdoor, Indoor, & 'Cross Country' """
+    __tablename__ = "sports"
 
-    event_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    event_abbrev = db.Column(db.String(8), unique=True, nullable=False)
-    event_name = db.Column(db.String(20), unique=True, nullable=False)
-    event_type_id = db.Column(db.ForeignKey("event_types.event_type_id"),
-                              nullable=False)
-    # TODO: Not sure about the following column. Maybe this should be
-    # stored with Meet Info?
-    # event_order = db.Column(db.Integer, unique=True, nullable=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(15), nullable=False, unique=True)
+
+    meets = db.relationship("Meet")
 
     def __repr__(self):
-        return "EVENT ID#{}: {}".format(self.grade_code, self.grade_name)
-
-
-class Event_Type(db.Model):
-    """
-    """
-    __tablename__ = "event_types"
-
-    event_type_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    event_type_name = db.Column(db.String(8), unique=True, nullable=False)
-
-    def __repr__(self):
-        return "EVENT ID#{}: {}".format(self.grade_code, self.grade_name)
+        return "SPORT#{}: {}".format(self.id, self.name)
 
     @classmethod
-    def get_from_name(cls, event_type_string):
-        ev_type = cls.query.filter(cls.event_type_name == event_type_string).one()
-        return ev_type
-
-
-class DivisionEvent(db.Model):
-    """
-    Associative table to handle the many to many relationship between Events
-    and Divisions.
-    """
-    __tablename__ = "division_events"
-    div_event_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    division_id = db.Column(db.ForeignKey("divisions.division_id"),
-                            nullable=False)
-    event_id = db.Column(db.ForeignKey("events.event_id"),
-                         nullable=False)
-
-    # event = db.relationship("Event")
-    division = db.relationship("Division")
-    entries = db.relationship("Entry")
-
-
-class Entry(db.Model):
-    """
-    A middle table. The "entries" of athletes into divisionEvents. Each athlete
-    can enter multiple events within his/her division. Each division within an
-    event has multiple athletes.
-    Note that entering into an event doesn't mean the athlete will actually
-    get assigned to run in the event.""
-    """
-    __tablename__ = "entries"
-    entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    div_event_id = db.Column(db.ForeignKey("division_events.div_event_id"),
-                             nullable=False)
-    athlete_id = db.Column(db.ForeignKey("athletes.athlete_id"),
-                           nullable=False)
-    # mark = db.Column(db.Float, nullable=True)
-    # mark_date = db.Column(db.Datetime, nullable=True)
-
-    athlete = db.relationship("Athlete")
-    div_event = db.relationship("DivisionEvent")
-    # division = db.relationship("DivisionEvent.division")
-    # event = db.relationship("DivisionEvent.event")
+    def get_by_name(cls, name):
+        return cls.query.filter_by(name=name).one()
 
 
 ###############
--- populate_data.py
+++ populate_data.py
@@ -3,8 +3,9 @@ Populates our database with starter data
 Load up the "Constants" for our MVP's database.
 """
 
-from model import (Gender, Grade, Division, School, Event_Type, Event, Entry,
-                   Athlete, db, connect_to_db)
+from model import (Meet, Athlete, Entry, School, Division, Event_Definition,
+                   Event_Def_Type, MeetDivisionEvent, Gender, Grade, Sport,
+                   db, connect_to_db)
 
 
 EVENT_DEFS = ({"abbrev": "100M", "name": "100 Meter", "type": "sprint"},
@@ -15,32 +16,20 @@ EVENT_DEFS = ({"abbrev": "100M", "name": "100 Meter", "type": "sprint"},
               {"abbrev": "4x400M", "name": "4x400 Meter Relay", "type": "relay"},
               {"abbrev": "LJ", "name": "Long Jump", "type": "horzjump"},
               {"abbrev": "TJ", "name": "Triple Jump", "type": "horzjump"},
-              {"abbrev": "DS", "name": "Long Jump", "type": "horzjump"},
-              {"abbrev": "TJ", "name": "Triple Jump", "type": "horzjump"},
               {"abbrev": "DT", "name": "Discus Throw", "type": "throw"},
               {"abbrev": "SP", "name": "Shot Put Throw", "type": "throw"},
               {"abbrev": "HJ", "name": "High Jump", "type": "vertjump"})
 
+EVENT_DEF_TYPES = ("sprint", "distance", "relay", "vertjump", "horzjump", "throw")
 
-def populate_genders():
-    """ Setup Gender Tables with constant data for middle school"""
+SPORTS = ("Outdoor", "Indoor", "Cross Country")
 
-    male = Gender(gender_code="M", gender_name="Boys")
-    female = Gender(gender_code="F", gender_name="Girls")
-    db.session.add_all([male, female])
-    db.session.commit()
+GRADES = ("6", "7", "8")
 
-
-def populate_grades(lowest_grade, highest_grade):
-    """
-    Setup grades tables with constant data for numbered grades between
-    lowest_grade and highest_graade, inclusive
-    """
-
-    for i in range(lowest_grade, highest_grade+1):
-        gr = Grade(grade_code=str(i), grade_name="Grade {}".format(i))
-        db.session.add(gr)
-
+def populate_sports(sport_name_list):
+    for name in sport_name_list:
+        sport = Sport(name=name)
+        db.session.add(sport)
     db.session.commit()
 
 
@@ -71,53 +60,94 @@ def populate_schools():
     db.session.commit()
 
 
-def populate_event_types():
+def populate_event_def_types(event_def_type_list):
     """ """
-
-    for type in ("sprint", "distance", "relay", "vertjump", "horzjump", "throw"):
-        event_type = Event_Type(event_type_name=type)
-        db.session.add(event_type)
+    for type in event_def_type_list:
+        event_def_type = Event_Def_Type(name=type)
+        db.session.add(event_def_type)
     db.session.commit()
 
 
 def populate_event_defs(event_list):
-    populate_event_types()
 
     for ev in event_list:
-        # Need to GET the event_type id 
-        event_type = Event_Type.get_from_name(ev['type'])
-        event = Event(event_abbrev=ev['abbrev'],
-                      event_name=ev['name'],
-                      event_type_id=event_type.event_type_id)
-        db.session.add(event)
+        # Need to GET the event_type id
+        type = Event_Def_Type.get_from_name(ev['type'])
+        event_def = Event_Definition(abbrev=ev['abbrev'],
+                                     name=ev['name'],
+                                     event_type_id=type.id)
+        db.session.add(event_def)
+    db.session.commit()
+
+
+def populate_genders():
+    """ Setup Gender Tables with constant data for middle school"""
+
+    male = Gender(gender_code="M", gender_name="Boys")
+    female = Gender(gender_code="F", gender_name="Girls")
+    db.session.add_all([male, female])
+    db.session.commit()
+
+
+def populate_grades(grades_list):
+    """
+    Setup grades tables with constant data for numbered grades between
+    lowest_grade and highest_graade, inclusive
+    """
+
+    for grade_string in grades_list:
+        grade = Grade(grade_code=grade_string,
+                      grade_name="Grade {}".format(grade_string))
+        db.session.add(grade)
+    db.session.commit()
+
+
+def create_a_meet():
+    meet = Meet(name="My Test Meet", sport_id=1)
+    db.session.add(meet)
+    db.session.commit()
+    populate_meet_division_events(meet)
+    return meet
+
+
+def populate_meet_division_events(meet):
+    events = Event_Definition.query.all()
+    divisions = Division.query.all()
+
+    for event in events:
+        for division in divisions:
+            mde = MeetDivisionEvent(div_id=division.id,
+                                    meet_id=meet.id,
+                                    event_def_id=event.id)
+            db.session.add(mde)
     db.session.commit()
 
 
 def reset_start_data():
     # Delete all rows in tables, so if we need to run this seeding program
     # a second time, we won't be trying to add duplicate data
-    Entry.query.delete()
-    Athlete.query.delete()
-    School.query.delete()
-    Event_Type.query.delete()
-    Division.query.delete()
-    Grade.query.delete()
-    Gender.query.delete()
+    db.drop_all()
 
 
 def populate_start_data():
+    populate_sports(SPORTS)
     populate_genders()
-    populate_grades(6, 8)
+    populate_grades(GRADES)
     populate_divisions()
     populate_schools()
+    populate_event_def_types(EVENT_DEF_TYPES)
     populate_event_defs(EVENT_DEFS)
+    meet = create_a_meet()
+
+
 
 
 if __name__ == "__main__":
     from server import app
     connect_to_db(app, "tms-dev")
-    db.create_all()
     reset_start_data()
+    db.create_all()
+
     populate_start_data()
 
     print("Connected to DB")
