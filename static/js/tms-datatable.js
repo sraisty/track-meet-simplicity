c44211c7370c4cffab71670a63251f8e2c38aa62
Sue Raisty <sraisty@gmail.com>
2018-10-16 23:15:27 -0700

    new templates for MDE & school detail

---
+++ static/js/tms-datatable.js
@@ -0,0 +1,15 @@
+$('#meet_table').DataTable({
+    'ordering': true
+});
+
+$('#athlete_table').DataTable({
+    'ordering': true
+});
+
+$('#school_table').DataTable({
+    'ordering': true
+});
+
+$('#mde_table').DataTable({
+    'ordering': true
+});
