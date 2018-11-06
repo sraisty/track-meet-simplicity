77fd9f5a90aac665b9680445b57b963d508e6398
Sue Raisty <sraisty@gmail.com>
2018-11-06 09:25:34 -0800

    added some doc about the color pallete

---
+++ static/js/navbar.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function() {
+
+    /// Tables listing MDE entrants
+    // $('#home_navbar').DataTable({
+    //     'ordering': true
+    // });
+
+  //   $('#meets_navbar')
+  // $('#schools_navbar')
+  // $('#athletes_navbar')
+})
